58b4d8482aeea2b023945da9a5e0d016a374e57b
Georg Sokolov <georgviktorsokolov@gmail.com>
2022-11-21 10:14:17 +0200

    Viikko 2. Tehtävä 2

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,4 +1,6 @@
 class RatingsController < ApplicationController
     def index
+        @ratings = Rating.all
+        render :index  # renderöin näkymätemplate /app/views/ratings/index.html
     end
 end
