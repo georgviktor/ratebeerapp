d1b1cfa666e3b5d4bc1ca3660c28a2faaf78a233
Georg Sokolov <georgviktorsokolov@gmail.com>
2022-11-21 16:40:01 +0200

    v2t8

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,12 +1,12 @@
 class RatingsController < ApplicationController
     def index
         @ratings = Rating.all
-        @beers = Beer.all
         render :index  # renderöin näkymätemplate /app/views/ratings/index.html
     end
     
     def new
         @rating = Rating.new
+        @beers = Beer.all
     end
 
     def create
--- app/models/beer.rb
+++ app/models/beer.rb
@@ -5,4 +5,9 @@ class Beer < ApplicationRecord
     def average_score
         self.ratings.average(:score).to_f.truncate(1)
     end
+
+    def to_s
+        "#{self.name} #{Brewery.find_by(id:self.brewery_id).name}"
+    end
+
 end
