e9bb9a147905b17efd5cb89fbf5a3246c59736e1
Georg Sokolov <georgviktorsokolov@gmail.com>
2022-11-21 16:11:34 +0200

    v2t7ver2

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,6 +1,7 @@
 class RatingsController < ApplicationController
     def index
         @ratings = Rating.all
+        @beers = Beer.all
         render :index  # renderöin näkymätemplate /app/views/ratings/index.html
     end
     
